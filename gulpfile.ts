871b520ae1b7cdd76b9faf6dfbf57e96fe82012b
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2017-09-26 07:18:03 -0600

    add a couple of changes for my own project

---
+++ gulpfile.ts
@@ -0,0 +1,7 @@
+import * as gulp from 'gulp';
+import * as watch from 'gulp-watch';
+import * as uglify from 'gulp-uglify';
+import * as sass from 'gulp-sass';
+import * as tsc from 'gulp-typescript';
+import * as sourcemaps from 'gulp-sourcemaps';
+
